8c30ddbfa52326be1cfd9c833659f3aba9048e68
1cookspe <1cookspe@001E374FA31C.ihdsb.ca>
2014-09-18 09:50:42 -0400

    Added first program

---
+++ src/edu/hdsb/gwss/spencercook/ics3u/u3/HelloWorld.java
@@ -0,0 +1,30 @@
+/*
+ * Name: Spencer Cook
+ * Date: Sept 18, 2014
+ * Version: v1.
+ * Description: 
+        This code prints "Hello world" to the output window. 
+ */
+package edu.hdsb.gwss.spencercook.ics3u.u3;
+
+/**
+ *
+ * @author 1cookspe
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Comment
+        
+        /*
+        Commment, skip over
+        */
+        
+       System.out.println("Hello, World!");
+    }
+    
+}
