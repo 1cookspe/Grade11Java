d6bb3483179253b781c06a75c2a4ea08f6f3a940
1cookspe <1cookspe@001E374FA31C.ihdsb.ca>
2014-09-22 11:08:09 -0400

    Bingo Game Addition

---
+++ src/edu/hdsb/gwss/spencercook/ics3u/u3/BingoRockPaperScissorsSmile.java
@@ -0,0 +1,30 @@
+/*
+ * Name: Spencer Cook
+ * Date: September 22, 2014
+ * Version: v0
+ * Description:
+        This code creates a bingo, rock paper scissors, and smile application
+*/
+package edu.hdsb.gwss.spencercook.ics3u.u3;
+
+/**
+ *
+ * @author 1cookspe
+ */
+public class BingoRockPaperScissorsSmile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+//Exercise 1: The Bingo Game
+        System.out.format("%-5s| %-5s| %-5s| %-5s| %-5s", " B", " I", " N", " G", " O");
+        System.out.format("\n%-5s| %-5s| %-5s| %-5s| %-5s", " 8"," 67", " 42", " 3", " 99");
+        System.out.format("\n%-5s| %-5s| %-5s| %-5s| %-5s", " 75", " 51", " FREE", " 12", " 87");
+        System.out.format("\n%-5s| %-5s| %-5s| %-5s| %-5s", " 90", " 77", " 59", " 31", " 66");
+        System.out.format("\n%-5s| %-5s| %-5s| %-5s| %-5s", " 56", " 34", " 73", " 22", " 1");
+    }
+    
+}
