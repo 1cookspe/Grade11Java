840804523882b12c9c65f873979e545e26020ffa
1cookspe <1cookspe@001E374FA31C.ihdsb.ca>
2014-10-09 11:09:41 -0400

    More adjustments

---
+++ src/edu/hdsb/gwss/spencercook/ics3u/u3/StartEnd.java
@@ -0,0 +1,22 @@
+/* Name: Spencer Cook
+ * Date: October 8, 2014
+ * Version: v0
+ * Description:
+ This program reads a mass of a letter and gives the cost of mailing a first or second cost letter having that mass. 
+ */
+package edu.hdsb.gwss.spencercook.ics3u.u3;
+
+/**
+ *
+ * @author 1cookspe
+ */
+public class StartEnd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Constants
+    }
+    
+}
