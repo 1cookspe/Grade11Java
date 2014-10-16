b42c09cb4785ffa25630107865b9defef6d33d9d
spencercook <spencercook@spencers-mbp>
2014-10-16 19:41:50 -0400

    pic framing

--- src/edu/hdsb/gwss/spencercook/ics3u/u3/PictureFraming.java
+++ src/edu/hdsb/gwss/spencercook/ics3u/u3/PictureFraming.java
@@ -19,26 +19,53 @@ public class PictureFraming {
      */
     public static void main(String[] args) {
         // Variables
-        double M = 5;
-        double N = 10;
+        double M = 1;
+        double N = 1;
         double P = 1;
         double Q = 1;
+        String formattedWidth = "";
+        String formattedLength = "";
+        String formattedMatWidth = "";
+        String formattedMatLength = "";
+        String formattedPicWidth = "";
+        String formattedPicLength = "";
+
+        //Constants
+        final double FRAME_WIDTH = (N + (Q * 2) + (P * 2)) - 1;
+        final double FRAME_LENGTH = (M + (Q * 2) + (P * 2)) - 2;
+        final double MAT_WIDTH = (N + (P * 2)) - 1;
+        final double MAT_LENGTH = (M + (Q * 2)) - 1;
 
         //Objects
         Scanner frameInput = new Scanner(System.in);
 
-        //Calculations
-        // - Print out dimensions of frame
-        for (double widthOfMat = 1; widthOfMat <= P + N; widthOfMat++) {
-            //System.out.format("\n%4s", "+");
-        }
-        for (double lengthOfPicture = 1; lengthOfPicture <= M; lengthOfPicture++) {
-            System.out.format("%5s", ".");
-        }
-        for (double widthOfPicture = 2; widthOfPicture <= N; widthOfPicture++) {
-            for (int counter = 0; counter <= M; counter++) {
-                System.out.format("\n%5s", ".");
+        //CALCULATIONS
+        // - Find places of rectangle
+        for (double widthOfFrame = 0; widthOfFrame <= FRAME_WIDTH; widthOfFrame++) {
+//            System.out.println(widthOfFrame);
+            formattedWidth += "%1s";
+//            System.out.println(formattedWidth);
+            for (double lengthOfFrame = 0; lengthOfFrame <= FRAME_LENGTH; lengthOfFrame++) {
+                formattedLength += "%1s";
+//                System.out.println(formattedLength);
+                for (double widthOfMat = 0; widthOfMat <= MAT_WIDTH; widthOfMat++) {
+                    formattedMatWidth += "+";
+//                    System.out.println(formattedMatWidth);
+                    for (double lengthOfMat = 0; lengthOfMat <= MAT_LENGTH; lengthOfMat++) {
+                        formattedMatLength += "+";
+//                        System.out.println(formattedMatLength);
+                        for (double widthOfPic = 0; widthOfPic <= N; widthOfPic++) {
+                            formattedPicWidth += ".";
+                            for (double lengthOfPic = 0; lengthOfPic <= M; lengthOfPic++) {
+                                formattedPicLength += ".";
+                                System.out.format(formattedWidth, formattedWidth, formattedLength, formattedMatWidth, formattedMatLength, formattedPicWidth, formattedPicLength);
+                                //System.out.format(formattedLength, "");
+                            }
+                        }
+                    }
+                }
             }
+
         }
     }
 
