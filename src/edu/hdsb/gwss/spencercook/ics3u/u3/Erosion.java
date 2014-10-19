e4c6fa7a9ab0768b286588c54184435ef1c0733b
spencercook <spencercook@spencers-mbp>
2014-10-19 16:36:48 -0400

    Loop Assignments

---
+++ src/edu/hdsb/gwss/spencercook/ics3u/u3/Erosion.java
@@ -0,0 +1,22 @@
+/* Name: Spencer Cook
+ * Date: October 19, 2014
+ * Version: v0
+ * Description:
+ This program creates a loop that calculates how long it will take for Canada's topsoil to be eroded from 30cm to 9cm
+ */
+package edu.hdsb.gwss.spencercook.ics3u.u3;
+
+/**
+ *
+ * @author spencercook
+ */
+public class Erosion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Constants
+    }
+    
+}
