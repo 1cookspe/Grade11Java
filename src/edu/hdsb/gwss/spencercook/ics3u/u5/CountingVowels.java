2d923d4bfc8c116c1da5f4394330873f525a2e82
1cookspe <1cookspe@001E374FA31C.ihdsb.ca>
2014-10-27 09:49:37 -0400

    Please Mark SlotMachineGame2

---
+++ src/edu/hdsb/gwss/spencercook/ics3u/u5/CountingVowels.java
@@ -0,0 +1,35 @@
+/* Name: Spencer Cook
+ * Date: October 27, 2014
+ * Version: v0
+ * Description:
+ This counts the vowels in a sentence from the user
+ */
+package edu.hdsb.gwss.spencercook.ics3u.u5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 1cookspe
+ */
+public class CountingVowels {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Variables
+        String word = new String();
+
+        // Objects
+        Scanner input = new Scanner(System.in);
+
+        //INTRODUCE PROGRAM
+        System.out.println("Welcome to the Count Vowels Calculator!");
+        System.out.println("This program counts the number of vowels in a sentence.");
+        
+        //CALCULATE NUMBER OF VOWELS
+        
+    }
+
+}
