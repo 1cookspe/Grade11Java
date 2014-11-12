85f62f77a0bbaf138c211d85d2b98f6b64d15a90
1cookspe <1cookspe@001E374FA31C.ihdsb.ca>
2014-11-12 10:35:38 -0500

    Final Project Files

---
+++ src/edu/hdsb/gwss/spencercook/ics3u/u6/ArrayUtil.java
@@ -0,0 +1,65 @@
+/* Name: Spencer Cook
+ * Date: November 11, 2014
+ * Version: v0
+ * Description:
+ This class contains array methods that can be used by other files 
+ */
+package edu.hdsb.gwss.spencercook.ics3u.u6;
+
+/**
+ *
+ * @author 1cookspe
+ */
+public class ArrayUtil {
+
+    public static int getLowestValueAbove(int[] values, int aboveValue ) {
+        int lowestValue = values[0];
+        for (int i = 1; i < values.length; i++) {
+            if (values[i] < lowestValue && values[i] > aboveValue ) {
+                lowestValue = values[i];
+            }
+        }
+        return lowestValue;
+    }
+    
+    public static int getLowestValue(int[] values) {
+        int lowestValue = values[0];
+        for (int i = 1; i < values.length; i++) {
+            if (values[i] < lowestValue) {
+                lowestValue = values[i];
+            }
+        }
+        return lowestValue;
+    }
+
+    public static int getHighestValue(int[] values) {
+        int highestValue = values[0];
+        for (int i = 1; i < values.length; i++) {
+            if (values[i] > highestValue) {
+                highestValue = values[i];
+            }
+        }
+        return highestValue;
+    }
+    
+    public static double calculateAverage(int[] values) {
+        double sum = 0;
+        for (int i = 0; i < values.length; i++) {
+            sum += values[i];
+        }
+        return sum / values.length;
+    }
+    
+    public static double calculateNonZeroAverage(int[] values) {
+        double sum = 0;
+        int numberOfValues = 0;
+        for (int i = 0; i < values.length; i++) {
+            if( values[i] > 0 ) {
+                sum += values[i];
+                numberOfValues++;
+            }
+        }
+        return sum / numberOfValues;
+    }
+    
+}
--- src/edu/hdsb/gwss/spencercook/ics3u/u6/Golf.java
+++ src/edu/hdsb/gwss/spencercook/ics3u/u6/Golf.java
@@ -16,7 +16,7 @@ public class Golf extends javax.swing.JFrame {
 
     int currentHole = 1;
     int[] scores = new int[18];
-    
+
     /**
      * Creates new form Golf
      */
@@ -40,6 +40,9 @@ public class Golf extends javax.swing.JFrame {
         holesInputted = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         scoreCard = new javax.swing.JTextArea();
+        highestScore = new javax.swing.JLabel();
+        lowestScore = new javax.swing.JLabel();
+        averageScore = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -60,6 +63,15 @@ public class Golf extends javax.swing.JFrame {
         scoreCard.setRows(5);
         jScrollPane1.setViewportView(scoreCard);
 
+        highestScore.setForeground(new java.awt.Color(255, 0, 0));
+        highestScore.setText("Highest Score:");
+
+        lowestScore.setForeground(new java.awt.Color(255, 0, 0));
+        lowestScore.setText("Lowest Score:");
+
+        averageScore.setForeground(new java.awt.Color(255, 0, 0));
+        averageScore.setText("Average Score:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -79,7 +91,11 @@ public class Golf extends javax.swing.JFrame {
                                 .addGap(26, 26, 26)
                                 .addComponent(addToScorecard))
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(holesInputted)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(holesInputted)
+                                    .addComponent(highestScore)
+                                    .addComponent(lowestScore)
+                                    .addComponent(averageScore))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap(53, Short.MAX_VALUE))
@@ -94,14 +110,21 @@ public class Golf extends javax.swing.JFrame {
                     .addComponent(inputScoreText)
                     .addComponent(score, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(addToScorecard))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(41, 41, 41)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 322, Short.MAX_VALUE)
+                        .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(22, 22, 22)
                         .addComponent(holesInputted)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 322, Short.MAX_VALUE))
-                .addContainerGap())
+                        .addGap(36, 36, 36)
+                        .addComponent(highestScore)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(lowestScore)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(averageScore)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
 
         pack();
@@ -109,25 +132,30 @@ public class Golf extends javax.swing.JFrame {
 
     private void addToScorecardActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addToScorecardActionPerformed
         if (!score.getText().equals("")) {
-        //Add value to array
-        scores[currentHole-1] = Integer.parseInt( score.getText() );
-        scoreCard.setText( scoreCard.getText() + "Hole #" + currentHole + ":          " + scores[currentHole-1] + "\n");
-         scoreCard.setEditable(false);
-        
-        //Print out how many holes have be input 
-        holesInputted.setText("Holes Entered: " + currentHole);
-        currentHole++;
-        
-        score.setText("");
-        inputScoreText.setText( "Input Score on Hole #" + currentHole + ":");
-        
-        if( currentHole > 18 ) {
-            JOptionPane.showMessageDialog(this, "You have inputted all 18 holes.");
-            score.setEditable( false );
-            score.setEnabled( false );
-            inputScoreText.setText("All scores have been entered!");
-            addToScorecard.setEnabled(false);
-        }
+            //Add value to array
+            scores[currentHole - 1] = Integer.parseInt(score.getText());
+            scoreCard.setText(scoreCard.getText() + "Hole #" + currentHole + ":          " + scores[currentHole - 1] + "\n");
+            scoreCard.setEditable(false);
+
+            //Print out how many holes have be input 
+            holesInputted.setText("Holes Entered: " + currentHole);
+            currentHole++;
+
+            score.setText("");
+            inputScoreText.setText("Input Score on Hole #" + currentHole + ":");
+
+            if (currentHole > 18) {
+                JOptionPane.showMessageDialog(this, "You have inputted all 18 holes.");
+                score.setEditable(false);
+                score.setEnabled(false);
+                inputScoreText.setText("All scores have been entered!");
+                addToScorecard.setEnabled(false);
+            }
+            
+            //Call methods from ArrayUtil
+            highestScore.setText("Highest Score: " + ArrayUtil.getHighestValue(scores));
+            lowestScore.setText("Lowest Score: " + ArrayUtil.getLowestValueAbove(scores, 0 ) );
+            averageScore.setText("Average Score: " + ArrayUtil.calculateNonZeroAverage(scores));
         } else {
             JOptionPane.showMessageDialog(this, "Please input a valid score!");
         }
@@ -170,10 +198,13 @@ public class Golf extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton addToScorecard;
+    private javax.swing.JLabel averageScore;
     private javax.swing.JLabel golfCard;
+    private javax.swing.JLabel highestScore;
     private javax.swing.JLabel holesInputted;
     private javax.swing.JLabel inputScoreText;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lowestScore;
     private javax.swing.JTextField score;
     private javax.swing.JTextArea scoreCard;
     // End of variables declaration//GEN-END:variables
