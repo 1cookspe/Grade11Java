b50797ea605fd99e1d7bea39743f67c525cb1896
1cookspe <1cookspe@001E374FA31C.ihdsb.ca>
2014-11-24 09:48:23 -0500

    Stats

--- src/edu/hdsb/gwss/spencercook/ics3u/u6/TableSort.java
+++ src/edu/hdsb/gwss/spencercook/ics3u/u6/TableSort.java
@@ -2,9 +2,11 @@
  * Date: November 19, 2014
  * Version: v0
  * Description:
- This program sorts atable of values and prints it to the user
+ This program sorts a table of values and prints it to the user
  */
 package edu.hdsb.gwss.spencercook.ics3u.u6;
+
+import java.util.Scanner;
 
 /**
  *
@@ -17,8 +19,25 @@ public class TableSort {
      */
     public static void main(String[] args) {
         //Variables
-        String[] playersNames = { "Sidney Crosby", "Jakub Voracek", "Tyler Seguin", "Steven Stamkos", "Phil Kessel", "Filip Forsberg", "Vladimir Tarasenko", "Mark Giordano", "Tyler Johnson", "Claude Giroux",  "Rick Nash", "Patric Hornqvist", "Evgeni Malkin" };
-        //int[] indicies = new int[10];
+        String[] playersNames = {"Jakub Voracek", "Tyler Seguin", "Sidney Crosby", "Steven Stamkos", "Claude Giroux", "Vladimir Tarasenko", "Phil Kessel", "Patric Hornqvist", "Evgeni Malkin", "Ryan Johansen"};
+        String[] playersTeams = {"Flyers", "Stars", "Penguins", "Lightning", "Flyers", "Blues", "Maple Leafs", "Penguins", "Penguins", "Blue Jackets"};
+        int[] goals = {7, 15, 8, 14, 6, 12, 11, 10, 8, 7};
+        int[] assists = {22, 11, 18, 10, 18, 11, 12, 13, 15, 16};
+        int[] points = {29, 26, 26, 24, 24, 23, 23, 23, 23, 23};
+        double[] shootingPercentage = {10.6, 18.8, 13.6, 18.2, 6.7, 15.0, 15.1, 14.1, 15.7, 13.7};
+        int[] indicies = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        
+        //Objects
+        Scanner sortInput = new Scanner(System.in);
+
+        //PRINT ARRAY
+        System.out.format("%20s  | %15s  | %5s  | %7s  | %7s  | %15s", "Player Name", "Team", "Goals", "Assists", "Points", "Shooting Percentage (%)");
+        for (int i = 0; i < playersNames.length; i++) {
+            System.out.format("\n%20s  | %15s  | %5s  | %7s  | %7s  | %15s", playersNames[i], playersTeams[i], goals[i], assists[i], points[i], shootingPercentage[i]);
+        } 
+       
+        //USER INPUT; GET, SET
+        
     }
-    
+
 }
