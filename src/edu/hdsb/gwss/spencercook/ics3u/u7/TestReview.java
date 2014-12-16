d0c562284ef1ffaa4be8b542c577a420ca1cb2c8
spencercook <spencercook@spencers-mbp>
2014-12-15 22:53:12 -0500

    Test Review

---
+++ src/edu/hdsb/gwss/spencercook/ics3u/u7/TestReview.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.spencercook.ics3u.u7;
+
+import java.io.File;
+import nu.xom.Document;
+import nu.xom.Element;
+
+/**
+ *
+ * @author spencercook
+ */
+public class TestReview {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        File file = new File("fileName.xml");
+        Element root = new Element("root");
+        Document document = new Document(root);
+        
+        Element firstElement = new Element("firstElement");
+        Element secondElement = new Element("secondElement");
+        Element begin = new Element("element");
+        
+        begin.appendChild(firstElement);
+        begin.appendChild(secondElement);
+        
+        root.appendChild(begin);
+        
+        firstElement.appendChild("Hello");
+        secondElement.appendChild("Hey");
+        
+        System.out.println(document.toXML());
+
+    }
+
+}
